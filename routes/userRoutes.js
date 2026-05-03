af4f1c545b44c0aca06a6db717b3b833e0f61dfa
Dylan J.A <dylanjimenezalfaro.ml@gmail.com>
2026-05-03 14:42:19 -0600

    Add cedula lookup route for padron autocomplete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { userPost, getAuthenticatedUser, activateUser, resendActivation} = require("../controllers/userController");
+const { userPost, getAuthenticatedUser, activateUser, resendActivation, getCedulaInfo } = require("../controllers/userController");
 const { authenticateToken } = require("../controllers/authJWT");
 
-// POST - Register user
+// POST - Registrar usuario (valida cédula contra el padrón)
 router.post('/user', userPost);
 
-// GET - Authenticated user (requires token)
+// GET - Consultar cédula en el padrón (autocompletado en formulario de registro)
+router.get('/cedula/:cedula', getCedulaInfo);
+
+// GET - Usuario autenticado (requiere token)
 router.get('/auth/user', authenticateToken, getAuthenticatedUser);
 
 router.get('/activate/:token', activateUser);
