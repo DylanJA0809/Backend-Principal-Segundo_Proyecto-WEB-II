de9247f0433af838a94c0c346b969e00538fe1e6
emilyzuniga049 <emilyzuniga049@gmail.com>
2026-05-06 19:52:41 -0600

    add openai validation

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,6 +1,7 @@
 const Question = require("../models/question");
 const Answer = require("../models/answer");
 const Vehicle = require("../models/vehicle");
+const { containsContactInfo } = require("../utils/openai");
 
 const questionPost = async (req, res) => {
   try {
@@ -30,6 +31,13 @@ const questionPost = async (req, res) => {
       if (!existingAnswer) {
         return res.sendStatus(409);
       }
+    }
+
+    const hasContactInfo = await containsContactInfo(req.body.message);
+    if (hasContactInfo) {
+      return res.status(400).json({
+        message: "Tu mensaje contiene información de contacto personal! Por favor utiliza la plataforma solo para negociar."
+      });
     }
 
     const question = new Question({
